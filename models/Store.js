db322fdf3bf0ee505745667ceae91a6823a6070a
apiasak <apiasak@gmail.com>
2020-05-29 05:34:01 +0700

    Add resize and upload images

--- models/Store.js
+++ models/Store.js
@@ -32,7 +32,8 @@ const storeSchema = new mongoose.Schema({
       type: String,
       required: 'You must supply address!'
     }
-  }
+  },
+  photo: String
 })
 
 storeSchema.pre('save', function(next){
