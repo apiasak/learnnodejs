4afd61b43065bb51b645fba8086364daf6039085
apiasak <apiasak@gmail.com>
2020-06-01 17:15:38 +0700

    User Accounts and Authentication

---
+++ controllers/authController.js
@@ -0,0 +1,89 @@
+const passport = require('passport')
+const crypto = require('crypto')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+const promisify = require('es6-promisify')
+
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: 'Failed login!',
+  successRedirect: '/',
+  successFlash: 'You are now logged in!'
+})
+
+exports.logout = (req, res) => {
+  req.logout()
+  req.flash('success', 'You are now logged out')
+  res.redirect('/')
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  // check if uster is authenticated
+  if (req.isAuthenticated()) {
+    next()
+    return
+  }
+  req.flash('error', 'Oops you must be logged in to do that!')
+  res.redirect('/login')
+}
+
+exports.forgot = async (req, res) => {
+  // 1. See if user is exists
+  const user = await User.findOne({ email: req.body.email })
+  if(!user){
+    req.flash('error', 'No account with that email exists')
+    return res.redirect('/login')
+  }
+  // 2. Set reset tokens and expiry
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex')
+  user.resetPasswordExpires = Date.now() + 3600000 // 1 hour from now
+  await user.save()
+  // 3. Send them an email
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`
+  req.flash('success', `You have been emailed a password reset link ${resetURL}`)
+  // 4. Redirect to login page
+  res.redirect('/login')
+}
+
+exports.reset = async (req,res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  })
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired')
+    return res.redirect('/login')
+  }
+  // if there is a user show a reset password form
+  res.render('reset', { title: 'Reset your password' })
+}
+
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']){
+    next()
+    return
+  }
+  req.flash('error', 'Passwords do not match!')
+  res.redirect('back')
+}
+
+exports.update = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  })
+
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired')
+    return res.redirect('/login')
+  }
+
+  const setPassword = promisify(user.setPassword, user)
+  await setPassword(req.body.password)
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpires = undefined;
+  const updatedUser = await user.save()
+  await req.login(updatedUser)
+  req.flash('success', 'Nice your password has been reset. You are now logged in')
+  res.redirect('/')
+}
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const storeController = require('../controllers/storeController')
+const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
-router.get('/', catchErrors(storeController.getStore));
-router.get('/stores', catchErrors(storeController.getStore));
+router.get('/', catchErrors(storeController.getStore))
+router.get('/stores', catchErrors(storeController.getStore))
 
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore)
 router.post('/add',
   storeController.upload,
   catchErrors(storeController.resize),
-  catchErrors(storeController.createStore));
+  catchErrors(storeController.createStore))
 
 router.post('/add/:id', 
   storeController.upload,
   catchErrors(storeController.resize),  
-  catchErrors(storeController.updateStore));
+  catchErrors(storeController.updateStore))
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
@@ -24,4 +26,26 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
 router.get('/tags', catchErrors(storeController.getStoresbyTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoresbyTag))
 
-module.exports = router;
+router.get('/login', userController.loginForm)
+router.post('/login', authController.login)
+router.get('/register', userController.registerForm)
+
+// Validate
+// Register user
+// log user in
+router.post('/register', 
+  userController.validateRegister,
+  userController.register,
+  authController.login)
+
+router.get('/logout', authController.logout)
+
+router.get('/account', authController.isLoggedIn, userController.account)
+router.post('/account', catchErrors(userController.updateAccount))
+router.post('/account/forgot', catchErrors(authController.forgot))
+router.get('/account/reset/:token', catchErrors(authController.reset))
+router.post('/account/reset/:token', 
+  authController.confirmedPasswords, 
+  catchErrors(authController.update)
+)
+module.exports = router
